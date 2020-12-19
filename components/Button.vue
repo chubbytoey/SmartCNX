990653543f2a59f7993fd1865cca30dcbc90a628
chubbytoey <toey.sasikrang@gmail.com>
2020-12-19 11:40:04 +0700

    ADD Button component

---
+++ components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button>{{text}}
+  </button>
+</template>
+<script>
+export default {
+  props: {
+    text: {
+      type:String,
+      default: ''
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+button {
+  width: 140px;
+  height: 40px;
+  border: none;
+  font-size: 14px;
+  border-radius: 4px;
+  border:none;
+  color: #fff;
+  background-color: #0094FF;
+}
+</style>
