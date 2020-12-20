11f9719043c8c18419ba72dceedf678a61fd73f6
chubbytoey <toey.sasikrang@gmail.com>
2020-12-20 12:46:52 +0700

    ADD descript page

---
+++ pages/descript.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+<script>
+export default {
+  layout:'lineliff',
+  props: {
+    data: {
+      type:Object,
+      default:null
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
